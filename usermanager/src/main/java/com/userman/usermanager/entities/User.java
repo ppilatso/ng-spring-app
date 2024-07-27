7d1c153dc8b177175865bd21cd782823356257a6
phenyopilatso <pilatsophenyo@gmail.com>
2024-07-27 20:35:32 +0200

    create entities

---
+++ usermanager/src/main/java/com/userman/usermanager/entities/User.java
@@ -0,0 +1,50 @@
+package com.userman.usermanager.entities;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Users")
+public class User implements Serializable {
+  @Id
+  private String id;
+  private String username;
+  private String lastname;
+
+  @DBRef
+  private Role role;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+}
