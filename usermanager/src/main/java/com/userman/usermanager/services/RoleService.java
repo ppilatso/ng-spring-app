4e8a05f5ea023b6208201e454eed27aa2431ad75
phenyopilatso <pilatsophenyo@gmail.com>
2024-07-27 20:53:35 +0200

    create services

---
+++ usermanager/src/main/java/com/userman/usermanager/services/RoleService.java
@@ -0,0 +1,34 @@
+package com.userman.usermanager.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.userman.usermanager.entities.Role;
+import com.userman.usermanager.repositories.RoleRepositories;
+
+public class RoleService {
+
+  @Autowired
+  RoleRepositories roleRepositories;
+
+  public Role createRole(Role role) {
+    return roleRepositories.save(role);
+  }
+
+  public Role getRoleById(String id) {
+    return roleRepositories.findById(id).orElse(null);
+  }
+
+  public List<Role> getAllRoles() {
+    return roleRepositories.findAll();
+  }
+
+  public Role updateRole(Role role) {
+    return roleRepositories.save(role);
+  }
+
+  public void deleteRole(String id) {
+    roleRepositories.deleteById(id);
+  }
+}
---
+++ usermanager/src/main/java/com/userman/usermanager/services/UserService.java
@@ -0,0 +1,34 @@
+package com.userman.usermanager.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.userman.usermanager.entities.User;
+import com.userman.usermanager.repositories.UserRepository;
+
+public class UserService {
+
+  @Autowired
+  UserRepository userRepository;
+
+  public User createUser(User user) {
+    return userRepository.save(user);
+  }
+
+  public User getUserById(String id) {
+    return userRepository.findById(id).orElse(null);
+  }
+
+  public List<User> getAllUsers() {
+    return userRepository.findAll();
+  }
+
+  public User updateUser(User user) {
+    return userRepository.save(user);
+  }
+
+  public void deleteUser(String id) {
+    userRepository.deleteById(id);
+  }
+}
